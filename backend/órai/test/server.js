8130617020d1b5315457000e03f4c4816851c98e
andrasvas <asulismodszer@gmail.com>
2025-01-24 14:19:06 +0100

    Update server.js

--- backend/órai/test/server.js
+++ backend/órai/test/server.js
@@ -17,6 +17,22 @@ app.get("/", (req, res) => {
     res.send("Fut a backend.")
 })
 
+app.get("/regiok", (req, res) => {
+    const sql = "SELECT * FROM `regiok`"
+    db.query(sql, (err, result) =>{
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+app.get("/alfold", (req, res) => {
+    const sql = "SELECT * FROM `regiok` WHERE Rid = 8"
+    db.query(sql, (err, result) =>{
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
 app.listen(3001, () => {
     console.log("Server is running on port 3001")
 })
