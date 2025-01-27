ff843a1b4fe803003b6720217f8fd5d0682ac6d2
andrasvas <asulismodszer@gmail.com>
2025-01-27 11:31:11 +0100

    post metodus

--- backend/órai/test/server.js
+++ backend/órai/test/server.js
@@ -25,11 +25,38 @@ app.get("/regiok", (req, res) => {
     })
 })
 
-app.get("/alfold", (req, res) => {
+app.get("/regiok_8", (req, res) => {
     const sql = "SELECT * FROM `regiok` WHERE Rid = 8"
     db.query(sql, (err, result) =>{
         if(err) return res.json(err);
         return res.json(result)
+    })
+})
+
+app.post("/ujregio", (req, res) =>{
+    const sql = "INSERT INTO `regiok` (`Rid`, `regionev`, `regio_tipusa`) VALUES ('',?,?)"
+    const values = ['11', 'Budapest', 'Főváros']
+
+    db.query(sql,values, (err, result) => {
+        if(err){
+            console.error("hiba történt", err)
+            return res.status(500).json({error: "Adatbázis hiba történt"})
+        }
+        return res.status(200).json({message: "Sikeres beszúrás!", result})
+    })
+})
+
+app.post("/ujregiok", (req, res)=> {
+    const sql = "INSERT INTO `regiok` (`Rid`, `regionev`, `regio_tipusa`) VALUES ('', ?, ?)"
+    const values = [['12', 'Bugyi', 'Város'],['13','Hernád','Kisváros']]
+
+    db.query(sql,[values], (err,result) =>{
+        if(err){
+            console.error("hiba tortent",err)
+            return res.status(500).json({error: "hiba történt"})
+        }
+
+        return res.status(200).json({message:"sikeres beszúrás", result})
     })
 })
 
